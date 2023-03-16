d7aa7f75f0d4b155aab31ccf01639fa348c2f7ef
Radu Lepadatu <radulepa@gmail.com>
2023-03-16 18:50:45 +0200

    Created Patient Components

---
+++ src/components/Patient/CreatePatient.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>Create Patient Page </p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
---
+++ src/components/Patient/ListPatients.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>List Patients Page</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
