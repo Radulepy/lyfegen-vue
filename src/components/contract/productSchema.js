953cc880b172aa2ad8e8151dcc9e665a58628024
Radu Lepadatu <radulepa@gmail.com>
2023-03-17 20:25:41 +0200

    Added Product Schema

---
+++ src/components/contract/productSchema.js
@@ -0,0 +1,22 @@
+const products = {
+  "products": [
+    {
+      "name": "ABC vial 10mg/ml",
+      "units": {
+        "10": "1000",
+        "20": "1800",
+        "30": "2500"
+      }
+    },
+    {
+      "name": "ABC vial 20mg/ml",
+      "units": {
+        "10": "1500",
+        "20": "2700",
+        "30": "4100"
+      }
+    }
+  ]
+}
+
+export default products;
