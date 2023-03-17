37593988216d05aba42ae9b18c48b2ea7a769f35
Radu Lepadatu <radulepa@gmail.com>
2023-03-17 20:26:57 +0200

    Update router - changed final path

--- src/router/index.js
+++ src/router/index.js
@@ -3,24 +3,23 @@ import { createRouter, createWebHistory } from 'vue-router' //importin router, h
 import Home from '../views/Home';
 
 const routes = [
-    //* views:
+    //* Views:
     { path: '/', name: 'Home', component: Home },
     { path: '/contracts', name: 'Contracts', component: () => import('../views/Contracts.vue') },
     { path: '/patients', name: 'Patients', component: () => import('../views/Patients.vue') },
     { path: '/:pathMatch(.*)*', component: () => import('../views/error/404.vue') }, // page not found
 
     //* Contract components:
-    { path: '/new-contract', name: 'NewContract', component: () => import('../components/Contract/CreateContract.vue') },
-    { path: '/all-contracts', name: 'AllContracts', component: () => import('../components/Contract/ListContracts.vue') },
-    { path: '/contract-status', name: 'ContractStatus', component: () => import('../components/Contract/StatusContract.vue') },
+    { path: '/new-contract', name: 'NewContract', component: () => import('../components/contract/CreateContract.vue') },
+    { path: '/all-contracts', name: 'AllContracts', component: () => import('../components/contract/ListContracts.vue') },
 
     //* Patient components:
-    { path: '/new-patient', name: 'NewPatient', component: () => import('../components/Patient/CreatePatient.vue') },
-    { path: '/all-patients', name: 'AllPatients', component: () => import('../components/Patient/ListPatients.vue') },
+    { path: '/new-patient', name: 'NewPatient', component: () => import('../components/patient/CreatePatient.vue') },
+    { path: '/all-patients', name: 'AllPatients', component: () => import('../components/patient/ListPatients.vue') },
 ];
 
 const router = createRouter({
-    history: createWebHistory(), // createWebHashHistory adds a '#' before links to stop server handling index.html file: domain/#/brands/categroy 
+    history: createWebHistory(), 
     routes // routes: [...]
 })
 
